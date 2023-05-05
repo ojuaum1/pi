baeea7622803c5ec580226e0017b122347a5cdb3
joao Oliveira <joao.oliveiira.pda@gmail.com>
2023-05-05 18:15:43 -0300

    projeto interdicipliar

--- pi/calculadora.py
+++ pi/calculadora.py
@@ -7,16 +7,20 @@
 
 # Grupo QI+
 
-decimal = int(input('digite um numero decimal:'))
-base = int(input('qual a base? [2] [8] [16]: '))
-
 digitos = '0123456789ABCDEF'
 
-convertido = ''
+while True:
+    decimal = int(input('Digite um número decimal: '))
+    base = int(input('Qual a base? [2] [8] [16]: '))
 
-while decimal > 0:
-    convertido = digitos[decimal%base] + convertido
-    decimal = decimal//base
+    if base not in [2, 8, 16]:
+        print('Base inválida.')
+    else:
+        convertido = ''
+        while decimal > 0:
+            convertido = digitos[decimal % base] + convertido
+            decimal = decimal // base
+        print(convertido)
+        break
 
-    print(convertido)
 
